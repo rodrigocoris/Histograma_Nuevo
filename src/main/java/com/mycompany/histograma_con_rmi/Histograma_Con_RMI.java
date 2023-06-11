9d38b93fb235d00421cb7d4e3f71640582c243fe
Rodrigo Alejandro Ramos Lozano <85802575+rodrigocoris@users.noreply.github.com>
2023-06-11 13:06:33 -0600

    prueba1

---
+++ src/main/java/com/mycompany/histograma_con_rmi/Histograma_Con_RMI.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.histograma_con_rmi;
+
+
+public class Histograma_Con_RMI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
